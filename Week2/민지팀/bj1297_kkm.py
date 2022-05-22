cb7da26709dd417468b1f842f089d49d7d43012c
lby9906 <lby9906@naver.com>
2022-05-22 17:03:32 +0900

    week02 kmj 코멘트

--- Week2/민지팀/bj1297_kkm.py
+++ Week2/민지팀/bj1297_kkm.py
@@ -1,5 +1,5 @@
 d, h, w = input().split()                   #현우 :고생하셨습니다
-d = int(d)
+d = int(d)                                  #민지 : 고생하셨습니답
 h = int(h)
 w = int(w)
 r = d / ((h ** 2 + w ** 2) ** 0.5)
