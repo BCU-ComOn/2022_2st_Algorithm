37d77a97bf5fc7c7a55b256377f98146f2777ca3
Kim-Su-B <hbsubin1@naver.com>
2022-05-28 23:08:19 +0900

    3주차 김수빈

---
+++ Week3/수빈팀/bj3046_ksb.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//R2
+		Scanner in = new Scanner(System.in);
+		
+		int r1 = in.nextInt();
+		int r2 = in.nextInt();
+		
+		System.out.println(r2*2-r1);
+		
+
+	}
+
+}
