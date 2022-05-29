f376706f9af0eea6c9c62740acb688886eed4f0f
KImSuim <suim7171@naver.com>
2022-05-29 21:55:40 +0900

    수임 코멘트

---
+++ Week3/수아팀/bj2884_ksi.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int hour = sc.nextInt();
+		int minute = sc.nextInt();
+		int H , M = 0;
+		if(minute < 45) {
+			M = 60-(45-minute);
+			H = hour-1;
+			if(H<0) {
+				H = 23;
+			}
+		}
+		else {
+			H = hour;
+			M = minute - 45;
+		}
+
+		
+		System.out.println(H+" "+M);
+	}
+
+}
